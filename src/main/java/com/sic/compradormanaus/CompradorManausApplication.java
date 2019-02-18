41026d796159f5a4bbe32290c1be5b60ed0a95c8
Edney Silva <edney.silva@fit-tecnologia.org.br>
2019-02-18 13:00:38 -0400

    Project Started to BackEnd components

---
+++ src/main/java/com/sic/compradormanaus/CompradorManausApplication.java
@@ -0,0 +1,13 @@
+package com.sic.compradormanaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompradorManausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompradorManausApplication.class, args);
+	}
+
+}
